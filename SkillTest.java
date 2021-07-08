bd0c22dadab1fe42c6572668ce7f3c6997c418fc
Jeremy Kane <jeremykane1579@gmail.com>
2021-07-08 17:15:46 +0700

    Fixed some logic error on question 1

--- SkillTest.java
+++ SkillTest.java
@@ -7,12 +7,12 @@ public class SkillTest {
         nums.add(1);
         nums.add(4);
         nums.add(2);
-	nums.add(4);
-	nums.add(5);
-	nums.add(7);
-	nums.add(6);
-	nums.add(4);
-	nums.add(7);
+        // nums.add(4);
+        // nums.add(5);
+        // nums.add(7);
+        // nums.add(6);
+        // nums.add(4);
+        // nums.add(7);
         System.out.println(question1(nums));
 
     }
@@ -23,15 +23,25 @@ public class SkillTest {
         for(int i = 0; i < nums.size(); ++i){
             boolean notZero = true;
             for(int j = 0; j < nums.size(); ++j){
-                if(i != j){
-                    if(nums.get(i)-nums.get(j) < 0){
-                        notZero = false;
-                    }
+                if(nums.get(i)-nums.get(j) < 0){
+                    notZero = false;
                 }
             }
             if(notZero){
                 results.add(nums.get(i));
             }
+        }
+        return results;
+    }
+
+    //Question 2 to return a list of integer that is not equal to integer x if it's divided by other elements
+    private static ArrayList question2(ArrayList<Integer> nums, int x){
+        ArrayList<Integer> results = new ArrayList<Integer>();
+        for(int i = 0; i < nums.size(); ++i){
+            boolean notEqualToX = true;
+            // for (int j = 0; j < nums.size(); ++j){
+
+            // }
         }
         return results;
     }
